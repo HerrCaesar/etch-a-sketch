d7a744de6b4a75d609a4a30fe600bde424ab8861
Zac <zac4793@gmail.com>
2019-03-29 12:26:29 +0000

    Make paintbrushes increase opacity over time, to give variable intensity in one colour

--- script.js
+++ script.js
@@ -3,22 +3,20 @@ const container = document.querySelector('.container');
 const resolutionInputs = document.getElementsByName('resolution');
 const gridlinesBoxes = document.getElementsByName('gridlines');
 
-// Always get a appropriately sized grid and populate with default res on open. 
+// Always get an appropriately sized grid and populate with default res on open```
 let gridResolution = 50;
 setGridDimensions()
 populateGrid()
 
-/* Store where the mouse is down over the document. Use document over grid 
+/* Store where the mouse is releaded over the document. Use document over grid
    so that if user releases mouse outside grid it will toggle the bool.*/
-let clicked = false;
-document.addEventListener('mousedown', (event) => {
-  clicked = true;
-})
+let clicked = false, interval = 0;
 document.addEventListener('mouseup', (event) => {
   clicked = false;
+  clearInterval(interval);
 })
 
-// Calls to reset grid dimensions when user finishes resizing the window
+// Call function to reset grid dimensions when user finishes resizing window
 window.onresize = setGridDimensions;
 
 // Toggle gridlines when checkbox is clicked
@@ -29,6 +27,10 @@ gridlinesBoxes.forEach((gridlinesBox) => {
     })
   })
 })
+
+// Placeholder for letting user choose brush strength and colour
+let brushColour = 'black';
+let brushStrength = 0.2;
 
 // Detect when user's finished entering desired resolution and call to read it
 resolutionInputs.forEach((resolutionInput) => {
@@ -45,7 +47,7 @@ resolutionInputs.forEach((resolutionInput) => {
    if resolution has changed. */
 function readResolutionFromInput(event)
 {
-  let newGridResolution = Math.min(event.target.value, 100) || gridResolution;
+  let newGridResolution = Math.min(cell.value, 100) || gridResolution;
   if (newGridResolution !== gridResolution)
   {
     gridResolution = newGridResolution;
@@ -55,6 +57,7 @@ function readResolutionFromInput(event)
 
 /* Delete existing cells in grid and repopulate based on requested 
    resolution, adding appropriate event listeners. */
+let colouringTime = 0;
 function populateGrid()
 {
   container.style.gridTemplateColumns = 'repeat(' + gridResolution + ', 1fr)';
@@ -68,24 +71,32 @@ function populateGrid()
   for (let i = 0; i < gridResolution ** 2; i++)
   {
     newItem = document.createElement('div');
+    newItem.style.minHeight = '1px';
     newItem.setAttribute('style',
       'grid-column: ' + (Math.floor(i / gridResolution) + 1) + ' / span 1;'
       + 'grid-row: ' + (i % gridResolution + 1) + ' / span 1;'
     )
 
-    // 'Mousemove' not '-over' '-enter' to enable steady opacity increase
-    newItem.addEventListener('mousemove', (event) => {
+    // If mouse button is already down when cell is entered, begin colouring
+    newItem.addEventListener('mouseover', (event) => {
       if (clicked)
       {
-        throttled(200, colourCell(event));
+        clearInterval(interval);
+        paintAtIntervals(event.target);
       }
+    })
+
+    newItem.addEventListener('mousedown', (event) => {
+      clicked = true;
+      clearInterval(interval);
+      paintAtIntervals(event.target);
     })
 
     container.appendChild(newItem);
   }
 }
 
-// Set width and height (but not #cells) of grid, based on window size
+// Set width and height of grid (but not # of cells), based on window size
 function setGridDimensions()
 {
   let newDimensions = newGridDimensions()
@@ -111,21 +122,29 @@ function newGridDimensions()
   }
 }
 
-// Change colour of the cell clicked on/over
-function colourCell(event)
+/* Reset cell if it's not paintbrush colour, then, at appropriate intervals, 
+   call function that actually paints cell. */
+function paintAtIntervals(cell)
 {
-  event.target.classList.add('black');
+  if (cell.style.backgroundColor !== brushColour)
+  {
+    cell.style.opacity = 0;
+    cell.style.backgroundColor = brushColour;
+  }
+  if (cell.style.opacity != 1)
+  {
+    paintCell(cell);
+    interval = setInterval(paintCell, 200, cell);
+  }
 }
 
-// Throttle the frequency of events firing. Used for mousemove event.
-function throttled(delay, fn) {
-  let lastCall = 0;
-  return function (...args) {
-    const now = (new Date).getTime();
-    if (now - lastCall < delay) {
-      return;
-    }
-    lastCall = now;
-    return fn(...args);
+/* Paint cell (increase opacity). Clear interval to stop repeating if opacity 
+   is 1. (Also cleared when a(nother) mousedown or mouseover event fires.) */
+function paintCell(cell)
+{
+  cell.style.opacity = Math.min(+cell.style.opacity + brushStrength, 1);
+  if (cell.style.opacity == 1)
+  {
+    clearInterval(interval);
   }
 }
